57a191a7e719b612cb0f75b8bb8390817ea9c8ed
souravbsk <souravbsk01@gmail.com>
2023-10-07 12:02:06 +0600

    portfolio change

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
-import { FaDownload, FaEye, FaFacebook, FaGithub, FaLinkedin, FaStackOverflow } from "react-icons/fa";
+import {
+  FaDownload,
+  FaEye,
+  FaFacebook,
+  FaGithub,
+  FaLinkedin,
+  FaStackOverflow,
+} from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 import { Tilt } from "react-tilt";
-import CircleAnimation from "../CircleAnimation/CircleAnimation";
 import { Link } from "react-router-dom";
 // import resumePdf from "../../assets/Resume-pdf/React Js Developer Resume of sourav basak.pdf"
 
 const HeroSection = () => {
-
-
-
   const handleDownload = () => {
     const downloadLink = document.createElement("a");
     downloadLink.href = "/Full Stack Developer Resume of sourav basak.pdf";
     downloadLink.download = "Full Stack Developer Resume of sourav basak.pdf";
     downloadLink.click();
   };
-
 
   return (
     <section className="container" id="hero">
@@ -51,36 +53,81 @@ const HeroSection = () => {
                 />
               </h4>
 
-              <div className="flex  pt-5 md:pt-10 gap-3 flex-col md:flex-row">
-            
-                <button  onClick={handleDownload} className=" rounded-3xl border-2 text-md hover:border-[#aafaff] border-[#aafaff] btn  primary-btn font-bold text-transparent  bg-clip-text bg-gradient-to-r from-[#aafaff] to-[#b5acff]   flex items-center gap-3" to="https://drive.google.com/u/0/uc?id=19FNYWhxd5of0-tGV4vc5TPOyR6yPXI5v&export=download" >
-                  <FaDownload className="animate-bounce text-[#b5acff]"></FaDownload>DownLoad Resume
-               </button>
-                <Link to='https://drive.google.com/file/d/19FNYWhxd5of0-tGV4vc5TPOyR6yPXI5v/view' target="_blank">
-                <button className=" border-2 text-md hover:border-[#aafaff] border-[#aafaff] btn rounded-3xl  primary-btn font-bold text-transparent  bg-clip-text bg-gradient-to-r from-[#aafaff] to-[#b5acff] flex items-center gap-3">
-                  <FaEye className="text-[#b5acff]"></FaEye>View Resume
+              <div className="flex pt-5 md:pt-10 gap-3 flex-col md:flex-row">
+                <button
+                  onClick={handleDownload}
+                  className=" rounded-3xl border-2 text-md hover:border-[#aafaff] border-[#aafaff] btn  primary-btn font-bold text-transparent  bg-clip-text bg-gradient-to-r from-[#aafaff] to-[#b5acff]   flex items-center gap-3"
+                  to="https://drive.google.com/u/0/uc?id=19FNYWhxd5of0-tGV4vc5TPOyR6yPXI5v&export=download"
+                >
+                  <FaDownload className="animate-bounce text-[#b5acff]"></FaDownload>
+                  DownLoad Resume
                 </button>
+                <Link
+                  to="https://drive.google.com/file/d/19FNYWhxd5of0-tGV4vc5TPOyR6yPXI5v/view"
+                  target="_blank"
+                >
+                  <button className=" border-2 text-md hover:border-[#aafaff] border-[#aafaff] btn rounded-3xl w-full primary-btn font-bold text-transparent  bg-clip-text bg-gradient-to-r from-[#aafaff] to-[#b5acff] flex items-center gap-3">
+                    <FaEye className="text-[#b5acff]"></FaEye>View Resume
+                  </button>
                 </Link>
- 
               </div>
               <div>
                 <ul className="flex items-center mt-6 md:mt-12 gap-6">
-                  <li className="hover:-mt-5 duration-300"><Link to="https://github.com/souravbsk" target="_blank" className="text-3xl text-[#d3ceff]"><FaGithub></FaGithub></Link></li>
-                  <li className="hover:-mt-5 duration-300"><Link to="https://www.linkedin.com/in/souravbsk/" target="_blank" className="text-3xl text-[#d3ceff]"><FaLinkedin></FaLinkedin></Link></li>
-                  <li className="hover:-mt-5 duration-300"><Link to="https://www.facebook.com/sourav.Alien" target="_blank" className="text-3xl text-[#d3ceff]"><FaFacebook></FaFacebook></Link></li>
-                  <li className="hover:-mt-5 duration-300"><Link to="https://stackoverflow.com/users/21434261/sourav-basak" target="_blank" className="text-3xl text-[#d3ceff]"><FaStackOverflow></FaStackOverflow></Link></li>
-                  <li className="hover:-mt-5 duration-300"><Link to="https://stackoverflow.com/users/21434261/sourav-basak" target="_blank" className="text-3xl text-[#d3ceff]"><FiMail></FiMail></Link></li>
+                  <li className="hover:-mt-5 duration-300">
+                    <Link
+                      to="https://github.com/souravbsk"
+                      target="_blank"
+                      className="text-3xl text-[#d3ceff]"
+                    >
+                      <FaGithub></FaGithub>
+                    </Link>
+                  </li>
+                  <li className="hover:-mt-5 duration-300">
+                    <Link
+                      to="https://www.linkedin.com/in/souravbsk/"
+                      target="_blank"
+                      className="text-3xl text-[#d3ceff]"
+                    >
+                      <FaLinkedin></FaLinkedin>
+                    </Link>
+                  </li>
+                  <li className="hover:-mt-5 duration-300">
+                    <Link
+                      to="https://www.facebook.com/sourav.Alien"
+                      target="_blank"
+                      className="text-3xl text-[#d3ceff]"
+                    >
+                      <FaFacebook></FaFacebook>
+                    </Link>
+                  </li>
+                  <li className="hover:-mt-5 duration-300">
+                    <Link
+                      to="https://stackoverflow.com/users/21434261/sourav-basak"
+                      target="_blank"
+                      className="text-3xl text-[#d3ceff]"
+                    >
+                      <FaStackOverflow></FaStackOverflow>
+                    </Link>
+                  </li>
+                  <li className="hover:-mt-5 duration-300">
+                    <Link
+                      to="https://stackoverflow.com/users/21434261/sourav-basak"
+                      target="_blank"
+                      className="text-3xl text-[#d3ceff]"
+                    >
+                      <FiMail></FiMail>
+                    </Link>
+                  </li>
                 </ul>
               </div>
-              
             </div>
             <div data-aos="zoom-in-up" className="flex-1 relative">
-               <div className="box"></div>
-               <div className=" rounded-2xl text-transparent  bg-clip-text bg-gradient-to-r from-[#aafaff] to-[#b5acff] absolute -bottom-6 px-3 md:px-8 py-3  md:left-20 md:text-xl border-2 text-md border-[#aafaff] font-bold  backdrop-blur-2xl">
+              <div className="box"></div>
+              <div className=" rounded-2xl text-transparent  bg-clip-text bg-gradient-to-r from-[#aafaff] to-[#b5acff] absolute -bottom-6 px-3 md:px-8 py-3  md:left-20 md:text-xl border-2 text-md border-[#aafaff] font-bold  backdrop-blur-2xl">
                 <span className="text-2xl md:text-5xl">2+</span> Year
                 <br />
                 Experience
-               </div>
+              </div>
             </div>
           </div>
         </div>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,15 +2,12 @@ import React, { useCallback } from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
-import ParticleCanvas from "../components/ParticleCanvas/ParticleCanvas";
 
 const Main = () => {
- 
   return (
     <div className="bg-[#02132B] bg-no-repeat bg-cover overflow-hidden">
       <Header></Header>
       <main className="mt-24">
-       <ParticleCanvas />
         <Outlet></Outlet>
       </main>
       <Footer></Footer>
