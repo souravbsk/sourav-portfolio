ea42a48f5daccad6ebf918733d8c48fbd22d3f8a
souravbsk <souravbsk01@gmail.com>
2023-11-12 17:38:54 +0600

    skill grid upgrade

--- src/components/Skilles/Skilles.jsx
+++ src/components/Skilles/Skilles.jsx
@@ -4,16 +4,18 @@ import Skill from "../Skill/Skill";
 import SectionTitle from "../Shared/SectionTitle";
 
 const Skilles = () => {
-const [skills,setSkills] = skillDataLoader();
-console.log(skills);
-    
+  const [skills, setSkills] = skillDataLoader();
+  console.log(skills);
+
   return (
     <div id="skills" className="container pt-16 md:pt-32">
       <SectionTitle title="My Skills"></SectionTitle>
-      <div data-aos="flip-up" className="grid grid-cols-1 md:grid-cols-5 gap-6 md:gap-12 mt-7 md:mt-14">
-        {
-          skills.map(skill => <Skill key={skill.id} skill={skill}></Skill>)
-        }
+      <div
+        className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-6 md:gap-8 mt-7 md:mt-14"
+      >
+        {skills.map((skill) => (
+          <Skill key={skill.id} skill={skill}></Skill>
+        ))}
       </div>
     </div>
   );
